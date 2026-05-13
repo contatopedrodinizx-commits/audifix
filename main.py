39d54dc4f168424de1f66119f65838205b8b76a8
Diniz <joaopedrocarvalhodiniz@gmail.com>
2026-05-13 12:02:08 -0300

    AudiFix 4.1

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ import requests
 import tkinter as tk
 from tkinter import messagebox
 import webbrowser
-
-VERSAO_ATUAL = "4.0"
+#tst
+VERSAO_ATUAL = "4.2"
 
 URL_VERSAO = "https://raw.githubusercontent.com/contatopedrodinizx-commits/audifix/main/version.txt"
 
