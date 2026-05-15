3f461b25658980dd438def8e9fec2b55620dc82b
Diniz <joaopedrocarvalhodiniz@gmail.com>
2026-05-15 16:06:26 -0300

    Atualização 5.6 final

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog, messagebox, font as tkfont
 
 # ─── Versão ───────────────────────────────────────────────────────────────────
-VERSAO_ATUAL = "5.5"
+VERSAO_ATUAL = "5.6"
 GITHUB_REPO  = "contatopedrodinizx-commits/audifix"
 URL_DOWNLOAD = f"https://github.com/{GITHUB_REPO}/releases/latest"
 
